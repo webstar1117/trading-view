0e019253f85ac9d44efdd9ead06b1750d9f12d44
pwu960411 <koszha.voran@mail.ru>
2022-02-28 16:31:19 +0900

    sd

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, 'build');
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 80;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
    res.sendFile(path.join(__dirname, `build/index.html`))
